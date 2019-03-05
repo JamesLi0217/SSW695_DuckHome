a5a8f9c0b5d69f60cc1127dee2104aa747458673
pchen12567 <pchen12@stevens.edu>
2019-03-05 15:43:46 -0500

    Create 20 init users

---
+++ init_users.py
@@ -0,0 +1,45 @@
+from faker import Faker
+import random
+import string
+import pandas as pd
+
+
+def make_users():
+    fake = Faker()
+
+    chars = string.ascii_letters + string.digits
+    length = 8
+    tags = ['Dogs Ok', 'Cats OK', 'No Pets', 'No Smoking',
+            'Furnished', 'Wheelchair Access', 'Quiet', 'Public Traffic',
+            'Parking', 'Laundry']
+
+    name = fake.name()
+    email = fake.email()
+    pwd = ''.join([random.choice(chars) for _ in range(length)])
+    mobile = fake.phone_number()
+    gender = random.choice(['Male', 'Female'])
+    tag = random.choices(tags, k=random.randint(1, 6))
+
+    return {
+        "name": name,
+        "email": email,
+        "password": pwd,
+        "gender": gender,
+        "mobile": mobile,
+        "tag": tag
+    }
+
+
+def main():
+    df = pd.DataFrame(columns=['name', 'email', 'password', 'gender', 'mobile', 'tag'])
+
+    for i in range(20):
+        dic = make_users()
+        series = pd.Series(dic)
+        df = df.append(series, ignore_index=True)
+
+    df.to_csv('./Administration/init_users.csv')
+
+
+if __name__ == '__main__':
+    main()
