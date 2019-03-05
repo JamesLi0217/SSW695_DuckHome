f73f354ee1a563d5e6c62b2e9e4fdf43515a9588
Ziang Lin <zlin17@stevens.edu>
2019-03-05 16:55:20 -0500

    add a script to download images

--- img_download.py
+++ img_download.py
@@ -4,15 +4,12 @@ cur_path = os.curdir
 
 def get_img(file):
     f = open(file, 'r+')
-    count = 0
     for line in f.readlines():
         line_list = line.split(',')
         zpid = line_list[0]
         url = line_list[1]
         urllib.urlretrieve(url, f"{cur_path}/imgs/+{zpid}.jpg")
-        count += 1
-        if count == 10:
-            break
+
     f.close()
 
 if __name__ == '__main__':
